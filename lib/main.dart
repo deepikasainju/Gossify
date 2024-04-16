a583c4dd667a1b0ee27ddb6d665b2b0d4d7e58c9
Deepika Sainju <dpisaiyo@gmail.com>
2024-04-16 09:38:04 +0545

    Homepage

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Gossify',
+        theme: ThemeData(
+            appBarTheme: const AppBarTheme(
+              centerTitle: true,
+              elevation: 1,
+              iconTheme: IconThemeData(color: Colors.black),
+              titleTextStyle: TextStyle(
+                  color: Colors.black, fontWeight: FontWeight.normal, fontSize: 19),
+              backgroundColor: Colors.white,
+            )),
+        home: const HomeScreen());
+  }
+}
